41325d4377a48e8ed79d90c49e149b0c17f3a9d2
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-21 01:26:08 +0300

    authentication

---
+++ ai-saas/app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="flex items-center justify-center">
+            {children}
+        </div>
+    );
+}
+export default AuthLayout;
